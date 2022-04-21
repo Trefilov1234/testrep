162d5a36f1a97919d4d7354afab6e2521a257ad1
Trefilov1234 <trefilov.2002@inbox.ru>
2022-04-21 20:24:26 +0400

    add code

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 void main()
 {
+    int size = 10;
+    int n;
+    int* mas = new int[size];
+    cout << "введите n" << endl;
+    cin >> n;
+    size = 2 * n + 1;
+    int k = 1;
+    for (int i = 0; i < size; i++)
+    {
+        mas[i] = k;
+        if (k == n)
+        {
+            k = 0;
+        }
+        k++;
 
+    }
+    for (int i = 0; i < size; i++) cout << mas[i] << " ";
 }
